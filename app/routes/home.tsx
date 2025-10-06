ea2aafe0e470de162b4ad4ac2253c52451266171
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-10-06 22:11:08 +0700

    Update home route component with new meta information and enhanced layout. Added Navbar component and improved the main section with a background image and descriptive headings for better user engagement.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,24 @@
 import type { Route } from "./+types/home"
+import Navbar from "~/components/Navbar"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "RESUMIND" },
+    { name: "description", content: "Welcome to Ai Resume Analyzer App!" },
   ]
 }
 
 export default function Home() {
-  return <div>asdasd</div>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      </section>
+    </main>
+  )
 }
